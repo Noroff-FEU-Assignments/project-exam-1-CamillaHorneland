9c3234361684832d4e53721323b2f206dc67c72d
Camilla <104870685+CamillaHorneland@users.noreply.github.com>
2023-02-11 00:15:15 +0100

    modal
    Finally got the modal up.
    But cant get it away again with selector: window?

--- js/postspesific.js
+++ js/postspesific.js
@@ -1,14 +1,12 @@
 const postspesificContainer = document.querySelector(".postSpesific");
 const title = document.querySelector("title");
-const modalImage = document.getElementById("modalImage");
 const modalsContainer = document.querySelector(".modal");
-const imageContainer = document.querySelector(".blogImage");
 
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
-const url = "https://camillahorneland.no/slime-care/wp-json/wp/v2/posts/" + id + "?_embed";
+const url = "https://camillahorneland.no/slime-care/wp-json/wp/v2/posts/" + id + "?_embed&";
 
 console.log(id);
 console.log(url);
@@ -19,29 +17,37 @@ const fetchPost = async() => {
         const result = await response.json ();
         console.log(result);
 
-        
+
         postspesificContainer.innerHTML = "";
- 
-         
-        
+         document.title += `${result["title"]["rendered"]}`;
           postspesificContainer.innerHTML += 
               
           `<div class="post">
               <div class="name"><h2>${result["title"]["rendered"]}</h2></div>
               <div class="getCatogory btn2">${result["_embedded"]["wp:term"][0][0]["name"]}</div>
-                <div id="modalImage"class="blogImage"><img src='${result["_embedded"]["wp:featuredmedia"][0]["source_url"]}'</><a class="modal"></a></div>
+                <div id="modalImage"><img src='${result["_embedded"]["wp:featuredmedia"][0]["source_url"]}'</></div>
                 <div class="text"><p>${result["content"]["rendered"]}</p></div>
                 <div class="date"><p>${result["date"]}</p></div>
               </div>
           </div>`;
-          document.title += `${result["title"]["rendered"]}`;
 
-        //  imageContainer.onclick = function (){
-        // modalsContainer.innerHTML += `${result["_embedded"]["wp:featuredmedia"][0]["source_url"]}`;
-        // console.log("clicked")
-        //  }
+       
+          modalImage.onclick = function () {
+            modalsContainer.innerHTML = "";
+            modalsContainer.innerHTML += 
+            `<div class="bigImage"> 
+                <img src='${result["_embedded"]["wp:featuredmedia"][0]["source_url"]}'</>
+            </div>`;
+            modalsContainer.style.display = "flex";
+          }
          
-}catch (error) {
+          window.onclick = function(event) {
+        if (event.target == modalsContainer) {
+          modalsContainer.style.display = "none";
+        }}
+
+         
+    }catch (error) {
       console.log(error);
       postspesificContainer.innerHTML = message("error", error);
    }
