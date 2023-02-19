970c8ee746d08f6f60ca56c939995c666eb48c87
Camilla <104870685+CamillaHorneland@users.noreply.github.com>
2023-02-19 22:52:45 +0100

    comments
    working on
    "Allow users to submit comments on a blog post, and post this data to WordPress"

---
+++ js/comments.js
@@ -0,0 +1,12 @@
+const commentsContainer = document.querySelector(".comments");
+
+        //   function renderComments(comments) {
+        //     comments.forEach(comment => {
+        //       const commentElement = document.createElement('div');
+        //       commentElement.innerHTML = 
+        //       `<p>${comment.content.rendered}</p>
+        //        <p>By ${comment.author_name} on ${comment.date}</p>`;
+        //        commentsContainer.appendChild(commentElement);
+        //       });
+        //     }
+        //     renderComments(comments);
